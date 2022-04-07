32b62fdd15c37c11bbd5b143d6d1407259d2c240
epwalsh <epwalsh10@gmail.com>
2022-04-06 17:00:54 -0700

    client warns if current version is outdated

--- beaker/client.py
+++ beaker/client.py
@@ -1,11 +1,13 @@
 from typing import Optional
 
 import docker
+from cachetools import TTLCache, cached
 
 from .config import Config
 from .data_model import *
 from .exceptions import *
 from .services import *
+from .version import VERSION
 
 __all__ = ["Beaker"]
 
@@ -19,7 +21,11 @@ class Beaker:
     The easiest way to initialize a Beaker client is with :meth:`.from_env()`.
     """
 
-    def __init__(self, config: Config):
+    def __init__(self, config: Config, check_for_upgrades: bool = True):
+        # See if there's a newer version, and if so, suggest that the user upgrades.
+        if check_for_upgrades:
+            self._check_for_upgrades()
+
         self._config = config
         self._docker: Optional[docker.DockerClient] = None
 
@@ -36,6 +42,30 @@ class Beaker:
         # Ensure default workspace exists.
         if self._config.default_workspace is not None:
             self.workspace.ensure(self._config.default_workspace)
+
+    @staticmethod
+    @cached(cache=TTLCache(maxsize=10, ttl=5 * 60))
+    def _check_for_upgrades():
+        import warnings
+
+        import packaging.version
+        import requests
+
+        try:
+            response = requests.get(
+                "https://api.github.com/repos/allenai/beaker-py/releases/latest", timeout=1
+            )
+            if response.ok:
+                latest_version = packaging.version.parse(response.json()["tag_name"])
+                if latest_version > packaging.version.parse(VERSION):
+                    warnings.warn(
+                        f"You're using beaker-py v{VERSION}, "
+                        f"but a newer version (v{latest_version}) is available. "
+                        f"Please upgrade with `pip install --upgrade beaker-py`.",
+                        UserWarning,
+                    )
+        except (requests.exceptions.Timeout, requests.exceptions.ConnectionError):
+            pass
 
     @classmethod
     def from_env(cls, **overrides) -> "Beaker":
