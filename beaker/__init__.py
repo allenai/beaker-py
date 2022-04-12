39f20b6cb7a9891029792ae5013613b838a231e5
epwalsh <epwalsh10@gmail.com>
2022-04-11 20:47:13 -0700

    fix link to quick start

--- beaker/__init__.py
+++ beaker/__init__.py
@@ -12,7 +12,7 @@ For example, you can check who you are logged in as with
 .. important::
     In this example - and all other examples - ``beaker`` is an instance of the
     :class:`Beaker` client class, not the :mod:`beaker` module.
-    See `Quick start </quickstart.html>`_ to learn how to instantiate the client.
+    See `Quick start <./quickstart.html>`_ to learn how to instantiate the client.
 
 Organizations
 -------------
