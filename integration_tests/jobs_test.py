dad56582e5dcb004cc1b6f3e23a1c4cb7cb2b97b
epwalsh <epwalsh10@gmail.com>
2022-04-20 10:48:14 -0700

    split off job and experiment integration tests

---
+++ integration_tests/jobs_test.py
@@ -0,0 +1,28 @@
+import time
+
+from beaker import Beaker, ExperimentSpec, TaskSpec
+
+
+def test_job_stop_and_finalize(client: Beaker, experiment_name: str, beaker_cluster_name: str):
+    start = time.time()
+    spec = ExperimentSpec().with_task(
+        TaskSpec.new(
+            "main",
+            beaker_cluster_name,
+            docker_image="hello-world",
+        ),
+    )
+    print(f"Creating experiment {experiment_name}")
+    experiment = client.experiment.create(experiment_name, spec)
+    print("Waiting for jobs to register", end="")
+    while not experiment.jobs:
+        if time.time() - start > 360:
+            raise TimeoutError
+        time.sleep(2)
+        print(".", end="")
+        experiment = client.experiment.get(experiment.id)
+    print("\nStopping job")
+    client.job.stop(experiment.jobs[0])
+    print("Finalizing job")
+    job = client.job.finalize(experiment.jobs[0])
+    assert job.is_finalized
--- tests/experiment_test.py
+++ tests/experiment_test.py
@@ -7,8 +7,6 @@ from beaker import (
     DataMount,
     DatasetNotFound,
     DataSource,
-    ExperimentConflict,
-    ExperimentNotFound,
     ExperimentSpec,
     ImageNotFound,
     ImageSource,
@@ -60,46 +58,6 @@ def test_experiment_empty_results(client: Beaker, hello_world_experiment_id: str
 def test_experiment_spec(client: Beaker, hello_world_experiment_id: str):
     spec = client.experiment.spec(hello_world_experiment_id)
     assert isinstance(spec, ExperimentSpec)
-
-
-def test_experiment_create_wait_rename(
-    client: Beaker,
-    experiment_name: str,
-    alternate_experiment_name: str,
-    beaker_cluster_name: str,
-    hello_world_experiment_name: str,
-):
-    spec = ExperimentSpec(
-        tasks=[
-            TaskSpec(
-                name="main",
-                image=ImageSource(docker="hello-world"),
-                context=TaskContext(cluster=beaker_cluster_name),
-                result=ResultSpec(path="/unused"),  # required even if the task produces no output.
-            ),
-        ],
-    )
-    # Create the experiment.
-    experiment = client.experiment.create(experiment_name, spec)
-
-    # Wait for it to finish.
-    experiment = client.experiment.wait_for(experiment, timeout=60 * 3)[0]
-
-    # Get the logs.
-    logs = "".join([line.decode() for line in client.experiment.logs(experiment)])
-    assert logs
-
-    # Test experiment conflict error with rename.
-    with pytest.raises(ExperimentConflict):
-        client.experiment.rename(experiment, hello_world_experiment_name)
-
-    # Rename the experiment.
-    experiment = client.experiment.rename(experiment, alternate_experiment_name)
-    assert experiment.name == alternate_experiment_name
-
-    # Test experiment not found error.
-    with pytest.raises(ExperimentNotFound):
-        client.experiment.get(experiment_name)
 
 
 def test_create_experiment_image_not_found(
