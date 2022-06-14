f533059b5afbada1a32bd0e78b4bb944a740300f
Pete <epwalsh10@gmail.com>
2022-06-14 13:52:28 -0700

    Finish all required methods for `BufferedReaderWithProgress` (#128)

--- beaker/progress.py
+++ beaker/progress.py
@@ -1,6 +1,6 @@
 import io
 import time
-from typing import Optional, Tuple
+from typing import List, Optional, Tuple
 
 from rich.console import Console
 from rich.live import Live
@@ -54,27 +54,105 @@ class TaskStatusColumn(ProgressColumn):
 
 
 class BufferedReaderWithProgress(io.BufferedReader):
-    def __init__(self, buffered_reader: io.BufferedReader, progress: Progress, task_id: TaskID):
-        super().__init__(buffered_reader.raw)
-        self.buffered_reader = buffered_reader
+    def __init__(self, handle: io.BufferedReader, progress: Progress, task_id: TaskID):
+        super().__init__(handle.raw)
+        self.handle = handle
         self.progress = progress
         self.task_id = task_id
         self.total_read = 0
 
+    def __enter__(self) -> "BufferedReaderWithProgress":
+        self.handle.__enter__()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    @property
+    def closed(self) -> bool:
+        return self.handle.closed
+
+    def close(self):
+        self.handle.close()
+
+    def fileno(self):
+        return self.handle.fileno()
+
+    def flush(self):
+        self.handle.flush()
+
+    def isatty(self) -> bool:
+        return self.handle.isatty()
+
+    def readable(self) -> bool:
+        return self.handle.readable()
+
+    def seekable(self) -> bool:
+        return self.handle.seekable()
+
+    def writable(self) -> bool:
+        return False
+
     def peek(self, size: int = 0) -> bytes:
-        return self.buffered_reader.peek(size)
+        return self.handle.peek(size)
 
     def read(self, size: Optional[int] = None) -> bytes:
-        out = self.buffered_reader.read(size)
+        out = self.handle.read(size)
         self.progress.advance(self.task_id, len(out))
         self.total_read += len(out)
         return out
 
     def read1(self, size: int = -1) -> bytes:
-        out = self.buffered_reader.read1(size)
+        out = self.handle.read1(size)
         self.progress.advance(self.task_id, len(out))
         self.total_read += len(out)
         return out
+
+    def readinto(self, b):
+        n = self.handle.readinto(b)
+        self.progress.advance(self.task_id, n)
+        self.total_read += n
+        return n
+
+    def readinto1(self, b):
+        n = self.handle.readinto1(b)
+        self.progress.advance(self.task_id, n)
+        self.total_read += n
+        return n
+
+    def readline(self, size: Optional[int] = -1) -> bytes:
+        out = self.handle.readline(size)
+        self.progress.advance(self.task_id, len(out))
+        self.total_read += len(out)
+        return out
+
+    def readlines(self, hint: int = -1) -> List[bytes]:
+        lines = self.handle.readlines(hint)
+        for line in lines:
+            self.progress.advance(self.task_id, len(line))
+            self.total_read += len(line)
+        return lines
+
+    def seek(self, offset: int, whence: int = 0) -> int:
+        pos = self.handle.seek(offset, whence)
+        self.progress.update(self.task_id, completed=pos)
+        return pos
+
+    def tell(self) -> int:
+        return self.handle.tell()
+
+    @property
+    def raw(self):
+        return self.handle.raw
+
+    def detach(self):
+        return self.handle.detach()
+
+    def write(self, s) -> int:
+        raise io.UnsupportedOperation("write")
+
+    def writelines(self, lines):
+        raise io.UnsupportedOperation("write")
 
 
 def get_experiments_progress(quiet: bool = False) -> Progress:
