3bf344ecf3477bdfd3a8e49fd0dcd18483f7776c
epwalsh <epwalsh10@gmail.com>
2022-09-21 14:59:32 -0700

    Add `Beaker.dataset.upload()` for uploading raw bytes

--- tests/dataset_test.py
+++ tests/dataset_test.py
@@ -12,3 +12,8 @@ def test_dataset_get(client: Beaker, squad_dataset_name: str):
 
 def test_file_info(client: Beaker, squad_dataset_name: str, squad_dataset_file_name: str):
     client.dataset.file_info(squad_dataset_name, squad_dataset_file_name)
+
+
+def test_upload(client: Beaker, dataset_name: str):
+    ds = client.dataset.create(dataset_name, commit=False)
+    client.dataset.upload(ds, b"foo-bar", "foo-bar")
